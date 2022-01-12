464a3bcaff28c5cb0fdc12838b52a78e03b747b4
valdis <>
2022-01-12 17:50:16 +0300

    create client and server app

---
+++ NetCommunication/ClientApp/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+namespace ClientApp
+{
+    public class App
+    {
+        public static void Main(params string[] input)
+        {
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager client = new NetManager(listener);
+            client.Start();
+            var peer = client.Connect("localhost" /* host ip or name */, 9050 /* port */, "SomeConnectionKey" /* text key or NetDataWriter */);
+            listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) =>
+            {
+                Console.WriteLine("We got: {0}", dataReader.GetString(100 /* max length of string */));
+                dataReader.Recycle();
+            };
+
+            string mess = null;
+
+            Thread thread = new Thread(() => {
+                while (!"quit".Equals(mess))
+                {
+                    mess = Console.ReadLine();
+                    NetDataWriter writer = new NetDataWriter();
+                    writer.Put(mess);
+                    peer.Send(writer, DeliveryMethod.ReliableOrdered);
+                }
+            });
+
+            thread.Start();
+
+            while (!"quit".Equals(mess))
+            {
+                client.PollEvents();
+                Thread.Sleep(15);
+            }
+
+            client.Stop();
+        }
+    }
+
+}
+
--- NetCommunication/NetCommunication/Program.cs
+++ NetCommunication/NetCommunication/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+namespace ServerApp
+{
+    public class App
+    {
+        public static void Main(params string[] input)
+        {
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager server = new NetManager(listener);
+            server.Start(9050 /* port */);
+
+            listener.ConnectionRequestEvent += request =>
+            {
+                if (server.ConnectedPeersCount < 10 /* max connections */)
+                    request.AcceptIfKey("SomeConnectionKey");
+                else
+                    request.Reject();
+            };
+
+            listener.PeerConnectedEvent += peer =>
+            {
+                Console.WriteLine("We got connection: {0}", peer.EndPoint); // Show peer ip
+                NetDataWriter writer = new NetDataWriter();                 // Create writer class
+                writer.Put("Hello client!");                                // Put some string
+                peer.Send(writer, DeliveryMethod.ReliableOrdered);             // Send with reliability
+            };
+
+            listener.NetworkReceiveEvent += Listener_NetworkReceiveEvent;
+
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+                Thread.Sleep(15);
+            }
+            server.Stop();
+        }
+
+        private static void Listener_NetworkReceiveEvent(NetPeer peer, NetPacketReader reader, DeliveryMethod deliveryMethod)
+        {
+            Console.WriteLine($"We got mess from {peer.EndPoint}:'{reader.GetString(100)}'  ({deliveryMethod})");
+            reader.Recycle();
+        }
+    }
+
+}
+
